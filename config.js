1b52afcfc6b10044727b97895e927e820918a2b1
Karthick Ragavendran <karthickragavendran@Karthicks-MacBook-Pro.local>
2020-06-23 20:49:41 +0530

    Created get route

--- config.js
+++ config.js
@@ -1,1 +1,2 @@
 export const PORT = 8000;
+export const FILE_PATH = './data/users.json';
--- router.js
+++ router.js
@@ -1,9 +1,12 @@
 import { Router } from 'https://deno.land/x/oak/mod.ts';
+import getUsers from './controllers/get.js';
 
 const router = new Router();
 
 router.get('/', ({ response }) => {
 	response.body = 'Welcome to deno!';
 });
+
+router.get('/users', getUsers);
 
 export default router;
